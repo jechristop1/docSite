a6f4aec3ef494126f74b0f812189dca85f2db4f3
Joshua Christopherson <jechristop1@gmail.com>
2015-03-05 17:54:54 -0800

    worked on menu buttons

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    //$(".links").hover(function(){
+    //    $(this).css('border', '2px solid red');
+    //}, function(){
+    //    $(this).css('border', '2px solid white');
+    //});
+});
